a024235aa6a47a852c49557b849d25c35c8e977d
aya-mahmoud <36283963+aya-mahmoud@users.noreply.github.com>
2020-04-03 17:41:07 +0200

    adding store owner

---
+++ SWEsprint1/SWEsprint1/src/main/java/com/test/module/storeOwner.java
@@ -0,0 +1,11 @@
+package com.test.module;
+
+public class storeOwner extends user  {
+
+
+    public storeOwner(String userName, String email, String password) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+}
